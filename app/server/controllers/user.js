dfd191c5051a4c19d943a0d3104d129db4fd5ad1
https://github.com/King123zamain <emekakingsley1804@gmail.com>
2021-11-23 13:25:16 +0100

    All done

--- app/server/controllers/user.js
+++ app/server/controllers/user.js
@@ -5,47 +5,49 @@ const user =require("../services/user")
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
-  const programs = getPrograms();
-  const graduationYears= getGradYears();
+    const programs = getPrograms();
+    const graduationYears= getGradYears();
 
-  const error = req.flash("error");
-  const user = req.session.user;    
+    const error = req.flash("error");
+    const user = req.session.user;    
 
   res.render("Signup", {program: programs, graduationYear: graduationYears, err: error, us: user })
 });
 
 router.post('/signup', (req, res) => {
   const firstname= req.body.firstName;
-  const  lastname= req.body.lastName
-  
+ const  lastname= req.body.lastName
+
   const {email, password, program, matricNumber, graduationYear} = req.body;
-    
-  const check = user.create({firstname, lastname, email, password, matricNumber, program, graduationYear});
   
+  const check = user.create({firstname, lastname, email, password, matricNumber, program, graduationYear});
+
   if (check[0]){
-    req.session.user = check[1];
+     req.session.user = check[1];
     res.redirect("/");
   }
   else{
     req.flash("error", check[1]);
     res.redirect(303, '/signup');
   }
-        
+      
 });
 
-
 router.get("/login",(req,res)=>{
+
   const error = req.flash("error");
   const user = req.session.user; 
-  
+
   res.render("Login", {err: error, us: user})
-  
+
 })
-  
+
 router.post("/login",(req,res)=>{
+
   const {email, password} =req.body
+
   const check = user.authenticate(email,password)
-  
+
   if (check[0]){
     req.session.user = check[1];
     res.redirect("/");
@@ -54,7 +56,7 @@ router.post("/login",(req,res)=>{
     req.flash("error", check[1][0]);
     res.redirect(303, '/login');
   }
-    
+  
 })
 
 module.exports = router;
