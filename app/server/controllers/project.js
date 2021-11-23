0599d3fe32c43aa258d3a9b588fb18fbcb29864e
https://github.com/King123zamain <emekakingsley1804@gmail.com>
2021-11-23 13:29:57 +0100

    third commit

--- app/server/controllers/project.js
+++ app/server/controllers/project.js
@@ -4,21 +4,21 @@ const {create, getById} = require("../services/project")
 const user = require("../services/user")
 
 router.get('/projects/submit', (req, res) => {    
-const error = req.flash("error");    
+  const error = req.flash("error");    
 
   res.render("CreateProject", { err: error, us: req.session.user});
   !req.session.user && res.redirect("/login");
 });
 
 router.post('/projects/submit', (req, res) => {
-    const tags = req.body.tags.split(",");
-    const authors = req.body.authors.split(",");
-    const {name, abstract} = req.body;
-    const createdBy = req.session.user.id;
+  const tags = req.body.tags.split(",");
+  const authors = req.body.authors.split(",");
+  const {name, abstract} = req.body;
+  const createdBy = req.session.user.id;
 
-    const idcheck = create({name, abstract, authors, tags, createdBy});
+  const idcheck = create({name, abstract, authors, tags, createdBy});
 
-    if (idcheck[0] === true){
+  if (idcheck[0] === true){
     res.redirect("/");
   }
   else{
@@ -28,10 +28,10 @@ router.post('/projects/submit', (req, res) => {
   
 });
 
-
 router.get('/project/:id', (req, res) => {
-    const idCheck = getById(req.params.id)
-    res.render("Project", {us: req.session.user, dat: idCheck, dat2: user.getById(idCheck.createdBy)});
+
+  const idCheck = getById(req.params.id)
+  res.render("Project", {us: req.session.user, dat: idCheck, dat2: user.getById(idCheck.createdBy)});
 });
 
 module.exports = router;
--- app/server/controllers/user.js
+++ app/server/controllers/user.js
@@ -5,40 +5,40 @@ const user =require("../services/user")
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
-    const programs = getPrograms();
-    const graduationYears= getGradYears();
+  const programs = getPrograms();
+  const graduationYears= getGradYears();
 
-    const error = req.flash("error");
-    const user = req.session.user;    
+  const error = req.flash("error");
+  const user = req.session.user;    
 
   res.render("Signup", {program: programs, graduationYear: graduationYears, err: error, us: user })
 });
 
 router.post('/signup', (req, res) => {
-    const firstname= req.body.firstName;
-   const  lastname= req.body.lastName
+  const firstname= req.body.firstName;
+  const  lastname= req.body.lastName
   
-    const {email, password, program, matricNumber, graduationYear} = req.body;
+  const {email, password, program, matricNumber, graduationYear} = req.body;
     
-    const check = user.create({firstname, lastname, email, password, matricNumber, program, graduationYear});
+  const check = user.create({firstname, lastname, email, password, matricNumber, program, graduationYear});
   
-    if (check[0]){
-       req.session.user = check[1];
-      res.redirect("/");
-    }
-    else{
-      req.flash("error", check[1]);
-      res.redirect(303, '/signup');
-    }
+  if (check[0]){
+    req.session.user = check[1];
+    res.redirect("/");
+  }
+  else{
+    req.flash("error", check[1]);
+    res.redirect(303, '/signup');
+  }
         
 });
 
 
 router.get("/login",(req,res)=>{
-    const error = req.flash("error");
-    const user = req.session.user; 
+  const error = req.flash("error");
+  const user = req.session.user; 
   
-    res.render("Login", {err: error, us: user})
+  res.render("Login", {err: error, us: user})
   
 })
   
